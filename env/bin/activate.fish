4787850bb2b121377fba010bd3b65c7206ae728f
APPLE <ilemobayosamson@gmail.com>
2021-03-23 00:05:53 +0100

    Send money Modification

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ittrade/PycharmProjects/payment/env"
+set -gx VIRTUAL_ENV "/Users/apple/Documents/Project/payment/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
